91f8fceeee56e4646f3ef92bddc3388fb36e1815
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2011-02-03 18:40:06 +0000

    Update copyright to 2011.
    git-svn-id: file:///Users/tom/Code/svn/repos/Win32-Process-Info/trunk@2131 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Win32/Process/Info.pm
+++ lib/Win32/Process/Info.pm
@@ -949,7 +949,7 @@ Thomas R. Wyant, III (F<wyant at cpan dot org>)
 Copyright (C) 2001-2005 by E. I. DuPont de Nemours and Company, Inc. All
 rights reserved.
 
-Copyright (C) 2007-2010 by Thomas R. Wyant, III
+Copyright (C) 2007-2011 by Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
